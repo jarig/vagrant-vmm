dd2588c9603af243d16c4746d8e63afc2e34cf47
jagorjat <jaroslav.gorjatsev@skype.net>
2015-01-30 16:37:29 +0200

    bugfix: v1.0.7 bugfix for fileshare creation, local users and sharing for Everyone doesn't work in domains

--- lib/vagrant-vmm/scripts/sync_folders.ps1
+++ lib/vagrant-vmm/scripts/sync_folders.ps1
@@ -144,12 +144,12 @@ if ( $sync_required )
       Write-host "$(&hostname) :: Creating fileshare on the remote host in $fileshare_dest, granting access to Everyone"
       $shr = New-SmbShare -Name "vagrant-sync" -Temporary -Path $fileshare_dest
     }
-    $g_info = Grant-SmbShareAccess -InputObject $shr -AccountName Everyone -AccessRight Full -Force -Confirm
+    $g_info = Grant-SmbShareAccess -InputObject $shr -AccountName "$($env:USERDOMAIN)\$($env:USERNAME)" -AccessRight Full -Force
   }
 
   # get access to the fileshare from the current machine
   Write-host "Getting access from the current machine to the created fileshare (\\$vm_address\vagrant-sync)"
-  $vagrant_sync_drive = New-PSDrive -Name 'V' -PSProvider 'FileSystem' -Root "\\$vm_address\vagrant-sync"
+  $vagrant_sync_drive = New-PSDrive -Name 'V' -PSProvider 'FileSystem' -Root "\\$vm_address\vagrant-sync" -Credential $creds_to_vm
 
   Write-host "Syncing files to fileshare..."
   foreach ( $hst_path in $copy_files.Keys )
--- lib/vagrant-vmm/version.rb
+++ lib/vagrant-vmm/version.rb
@@ -1,5 +1,5 @@
 module VagrantPlugins
   module VMM
-    VERSION = "1.0.6"
+    VERSION = "1.0.7"
   end
 end
